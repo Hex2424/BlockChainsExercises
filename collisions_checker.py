2fa00e4ee3112956c75391337d9486b80c3fd403
Markas Vielavicius <maildetectionteam@gmail.com>
2022-09-28 22:31:52 +0300

    collisions checker script

---
+++ collisions_checker.py
@@ -0,0 +1,24 @@
+import subprocess
+
+
+hashmap = {}
+
+wordlist = open("LT-Top500k.txt", 'r')
+lines = wordlist.readlines()
+
+for i in range(len(lines)):
+
+    if "#" in lines[i]:
+        continue
+
+    
+
+    proc = subprocess.Popen("./build/HashAlgorithm " + lines[i],shell=True, stdout=subprocess.PIPE)
+    output = proc.stdout.read()
+    # print(output)
+
+    if hashmap.get(output) is not None:
+        print("repeated now:" + lines[i] + "Before "+ hashmap.get(output))
+
+    else:
+        hashmap[output] = lines[i]
