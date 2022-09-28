28ebdaf6dac344e9655d0f9a2eb2cbb6818b7760
Markas Vielavicius <maildetectionteam@gmail.com>
2022-09-28 22:32:14 +0300

    added file read option

--- main/main.c
+++ main/main.c
@@ -12,7 +12,8 @@
  */
 #include "../hash/hash.h"
 #include "stdio.h"
-
+#include "string.h"
+#include "stdlib.h"
 
 #define EXIT_SUCCESS            0
 #define EXIT_NULL               -1
@@ -25,27 +26,81 @@
 
 int main(int args, char** argv)
 {
-    char outputHash[HASH_SIZE_BYTES];
+    char outputHash[HASH_SIZE_BYTES] = {0};
+    char* input = NULL;
 
-    // if(args == 1 || args > 2)
-    // {
-    //     printf("Program takes 1 argument - text to hash\n");
-    //     return EXIT_SUCCESS;
-    // }
-    // if(argv[1] == NULL)
-    // {
-    //     return EXIT_NULL;
-    // }
+    if(args == 1)
+    {
+        printf("Program takes 1 argument - text to hash\n");
+        return EXIT_SUCCESS;
 
-    if(!EHash_hash("575", outputHash, HASH_SIZE_BYTES))
+    }
+
+
+    if(args > 2)
+    {
+        if(strcmp(argv[1], "-f") == 0) // failo ivedimo argumento patikra
+        {
+            FILE* file = fopen(argv[2], "r");
+
+            if(file == NULL)
+            {
+                printf("Could not find mentioned file: %s\n", argv[2]);
+                return EXIT_SUCCESS;
+            }
+
+
+            fseek(file,0L, SEEK_END);
+            int fileSize = ftell(file);
+            rewind(file);
+            input = malloc(fileSize);               // putting input to RAM
+
+            if(input == NULL)
+            {
+                printf("Failed to allocate buffer for file size: %d\n", fileSize);
+                return EXIT_SUCCESS;
+            }
+
+            if(fread(input, 1, fileSize, file) != fileSize)
+            {
+                printf("Failed to read mentioned file: %s\n", argv[2]);
+                return EXIT_SUCCESS;
+            }
+        }else
+        {
+            printf("Program takes 1 argument - text to hash\n");
+            return EXIT_SUCCESS;
+        }
+    }
+
+    if(input == NULL)
+    {
+        input = argv[1];
+    }
+  
+    if(argv[1] == NULL)
+    {
+        return EXIT_NULL;
+    }
+
+    //  for(int i = 0; i < HASH_SIZE_BYTES; i++)
+    // {
+    //     printf("%02X ", (uint8_t) outputHash[i]);
+    // }
+    // printf("\n");
+
+    if(!EHash_hash(input, outputHash, HASH_SIZE_BYTES))
     {
         printf("Hashing got failed for some reason\n");
         return EXIT_HASH_FAIL;
     }
 
-
-
-    printf("eHash: %s\n", outputHash);
+    // for(int i = 0; i < HASH_SIZE_BYTES; i++)
+    // {
+    //     printf("%d ", outputHash[i]);
+    // }
+    printf("%s\n", outputHash);
+    // printf("eHash: %s\n", outputHash);
 
     return EXIT_SUCCESS;
 }
